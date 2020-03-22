6d6ad18cf64778daf79e45cec429157e24611e0c
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-03-22 12:57:46 +0530

    admin page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 # def about(request): 
 #     return HttpResponse("<h1>Tirth's Blog About</h1>")
 
-posts = [
-    {
-        'author': 'Tirth',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted': 'March 21, 2020'
-    },
-    {
-        'author': 'Rohit',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted': 'March 23, 2099'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Tirth',
+#         'title' : 'Blog Post 1',
+#         'content' : 'First post content',
+#         'date_posted': 'March 21, 2020'
+#     },
+#     {
+#         'author': 'Rohit',
+#         'title' : 'Blog Post 2',
+#         'content' : 'Second post content',
+#         'date_posted': 'March 23, 2099'
+#     }
+# ]
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
 
     }
     return render(request, 'blog/home.html', context)
