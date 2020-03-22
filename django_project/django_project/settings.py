e1f3e060674815e024157b4cc2747b5abe8d8d51
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-03-22 13:53:55 +0530

    register.html

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',  # added in v6
     'blog.apps.BlogConfig',  # added
     'django.contrib.admin',
     'django.contrib.auth',
