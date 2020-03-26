421a5ff0baf04ee328a8493d40f47c3084f2a915
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-03-26 16:30:50 +0530

    password_reset_12

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -130,3 +130,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'  # added
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BASKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
