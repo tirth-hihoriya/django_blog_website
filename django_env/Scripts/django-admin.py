683de5d177a428cfe7674a1c27345b388e555453
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-03-27 13:36:24 +0530

    changes for git

--- django_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\tirth hihoriya\google drive\4th sem\django_blog_website\django_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
