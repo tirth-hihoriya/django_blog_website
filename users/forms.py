0e84d6d16ac1f31d9b047543c4f6cf19438dedf5
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-09 14:39:59 +0530

    run black

--- users/forms.py
+++ users/forms.py
@@ -9,17 +9,18 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
-        
-  
+        fields = ["username", "email", "password1", "password2"]
+
+
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()  # default : required=True
 
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ["username", "email"]
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ["image"]
