45fe2f6c586e0f527b2595fffb98bb4f8dfafc45
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-09 15:44:33 +0530

    add forms for user and seller

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
     def __str__(self):
-        return f"{self.user.username} Profile"
+        return f"{self.user.username}"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
